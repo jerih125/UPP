5ebb5283c062ca663b387cb67555c9890c146e8c
jerih125 <jerih125@gmail.com>
2016-06-03 23:05:33 +0700

    New button added

--- UPP/Form2.Designer.cs
+++ UPP/Form2.Designer.cs
@@ -35,6 +35,7 @@
             this.btnEnter = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnRegistration = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -100,11 +101,22 @@
             this.btnRegistration.UseVisualStyleBackColor = true;
             this.btnRegistration.Click += new System.EventHandler(this.btnRegistration_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Здравствуйте";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 272);
+            this.ClientSize = new System.Drawing.Size(449, 284);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnRegistration);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnEnter);
@@ -129,5 +141,6 @@
         private System.Windows.Forms.Button btnEnter;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnRegistration;
+        private System.Windows.Forms.Button button1;
     }
 }
--- UPP/Form2.cs
+++ UPP/Form2.cs
@@ -141,5 +141,10 @@ namespace UPP
             registration.ShowDialog();
             Visible = true;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Добро пожаловать!");
+        }
     }
 }
